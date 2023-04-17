abf00627bf1153b14618c4fb66340a40c919f969
Mykyta Rudenko <mykyta.rudenko04@gmail.com>
2023-04-17 22:56:02 +0300

    created several texts for testing and added comments

--- Home_task_4/Exercise2/EmailAddressChecker.cs
+++ Home_task_4/Exercise2/EmailAddressChecker.cs
@@ -2,25 +2,35 @@
 
 namespace Exercise2;
 
+    // зробив не статичним классом з статичним методом,
+    // тому що потрібно повернути коректні адреси і ті,
+    // що містять в своєму записі символ "@", але не є коректними(мінімум 2 колекції)
 public class EmailAddressChecker
 {
     private List<string> _correctMails;
     private List<string> _notGoodMails;
+    public List<string> CorrectMails { get { return _correctMails; } }
+    public List<string> NotGoodMails { get { return _notGoodMails; } }
 
-    public EmailAddressChecker()
+    public EmailAddressChecker(string stringOfMails)
     {
         _correctMails = new List<string>();
         _notGoodMails = new List<string>();
+        CheckAddress(stringOfMails);
     }
-    public void CheckAddress(string mainStr)
+    private void CheckAddress(string text)
     {
-        string[] mailArray = mainStr.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-        foreach (var mail in mailArray)
+        string[] mails = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        int index;
+        foreach (var mail in mails)
         {
-            // if(IsValidEmailNetMethod(mail)) _correctMails.Add(mail);
-            // else if (ContainAtSymbol(mail)) _notGoodMails.Add(mail);
-            if (mail.IndexOf('@') != -1)
+            if ((index = mail.IndexOf('@')) != -1)
             {
+                if (mail.IndexOf('@', index + 1) != -1)
+                {
+                    _notGoodMails.Add(mail);
+                    continue;
+                }
                 if (IsValidMail(mail))
                 {
                     _correctMails.Add(mail);
@@ -30,52 +40,55 @@ public class EmailAddressChecker
             }
         }
     }
-    public bool IsValidEmailNetMethod(string email)
+    
+    // можна було реалізувати перевірку просто за допомогою System.Net.Mail.MailAddress
+    
+    // public bool IsValidEmailNetMethod(string email)
+    // {
+    //     try
+    //     {
+    //         var addr = new System.Net.Mail.MailAddress(email);
+    //         return addr.Address == email;
+    //     }
+    //     catch
+    //     {
+    //         return false;
+    //     }
+    // }
+    public bool IsValidMail(string mail)
     {
+        string[] domens = mail.Split('@');
         try
         {
-            var addr = new System.Net.Mail.MailAddress(email);
-            return addr.Address == email;
+            char firstSymbol = domens[0][0];
+            char lastSymbol = domens[0][domens[0].Length - 1];
+            if (firstSymbol == '.' || lastSymbol == '.' || domens[0].Length > 64) return false;
+            foreach (var symbol in domens[0])
+            {
+                if (!((symbol >= '!' && symbol <= '9') || (symbol >= 'A' && symbol <= 'Z') ||
+                      (symbol >= '^' && symbol <= '~')))
+                {
+                    return false;
+                }
+            }
+            firstSymbol = domens[1][0];
+            lastSymbol = domens[1][domens[1].Length - 1];
+            if (firstSymbol == '.' || lastSymbol == '-') return false;
+            foreach (var symbol in domens[1])
+            {
+                if (!((symbol >= '0' && symbol <= '9') || (symbol >= 'A' && symbol <= 'Z') ||
+                      (symbol >= 'a' && symbol <= 'z') || symbol == '-' || symbol == '.'))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
-        catch
+        catch (IndexOutOfRangeException ex)
         {
             return false;
         }
     }
-    public bool ContainAtSymbol(string mail)
-    {
-        if (mail.IndexOf('@') != -1) return true;
-        return false;
-    }
-    //TODO: create try catch
-    public bool IsValidMail(string mail)
-    {
-        string[] domens = mail.Split('@');
-        char firstSymbol = domens[0][0];
-        char lastSymbol = domens[0][domens[0].Length - 1];
-        if (firstSymbol == '.' || lastSymbol == '.') return false;
-        foreach (var symbol in domens[0])
-        {
-            if (!((symbol >= '!' && symbol <= '9') || (symbol >= 'A' && symbol <= 'Z') || (symbol >= '^' && symbol <= '~')))
-            {
-                return false;
-            }
-        }
-        
-        firstSymbol = domens[1][0];
-        lastSymbol = domens[1][domens[1].Length - 1];
-        if (firstSymbol == '.' || lastSymbol == '-') return false;
-        foreach (var symbol in domens[1])
-        {
-            if (!((symbol >= '0' && symbol <= '9') || (symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z') || symbol == '-' || symbol == '.'))
-            {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-
     public override string ToString()
     {
         StringBuilder sb = new StringBuilder();
--- Home_task_4/Exercise2/Program.cs
+++ Home_task_4/Exercise2/Program.cs
@@ -1,6 +1,14 @@
 ﻿using Exercise2;
 
-string textOfEmails = @"mykyta.rudenko04@gmail.com mail_mail@mail.com .hello@mail.com mail.@mail.com dudhfkd ...@gmail.ua Mykyta@mail fjhtjig@.";
-EmailAddressChecker emailChecker = new EmailAddressChecker();
-emailChecker.CheckAddress(textOfEmails);
+string textOfEmails = @"mykyta.rudenko04@gmail.com mail_mail@mail.com .hello@mail.com mail.@mail.com dudhfkd ...@gmail.ua Mykyta@mail fjhtjig@ @nure.ua abc.afaf.nure.ua";
+string textOfAllCorrectEmails = "simple@example.com very.common@example.com disposable.style.email.with+symbol@example.com other.email-with-hyphen@example.com fully-qualified-domain@example.com user.name+tag+sorting@example.com x@example.com example-indeed@strange-example.com admin@mailserver1 example@s.example \" \"@example.org \"john..doe\"@example.org mailhost!username@example.org user%example.com@example.org";
+string textOfAllUncorrectEmails = "Abc.example.com A@b@c@example.com a\"b(c)d,e:f;g<h>i[j\\k]l@example.com this is \"not\\allowed@example.com this\\ still\"not\\allowed@example.com 1234567890123456789012345678901234567890123456789012345678901234+x@example.com i_like_underscore@but_its_not_allow_in _this_part.example.com";
+EmailAddressChecker emailChecker = new EmailAddressChecker(textOfEmails);
 Console.WriteLine(emailChecker);
+Console.WriteLine(new string('-',50));
+emailChecker = new EmailAddressChecker(textOfAllCorrectEmails);
+Console.WriteLine(emailChecker);
+Console.WriteLine(new string('-',50));
+emailChecker = new EmailAddressChecker(textOfAllUncorrectEmails);
+Console.WriteLine(emailChecker);
+
