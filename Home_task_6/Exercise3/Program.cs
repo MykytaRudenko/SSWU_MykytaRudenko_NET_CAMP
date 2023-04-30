999257f2a20ce881c8b59dc00b51074a2306c047
Mykyta Rudenko <mykyta.rudenko04@gmail.com>
2023-05-01 01:32:16 +0300

    created class UniqueWordFinder and README file

---
+++ Home_task_6/Exercise3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using Exercise3;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+string text = "унікальні слова unique words, слова words\n наступна стрічка. Наступна фраза";
+var uniqueWords = UniqueWordFinder.FindWords(text);
+
+foreach (var item in uniqueWords)
+{
+    Console.Write(item + ", ");
+}
+
+Console.WriteLine('\n' + new string('-', 50));
+
+uniqueWords = UniqueWordFinder.SetFindWordsMethod(text);
+foreach (var item in uniqueWords)
+{
+    Console.Write(item + ", ");
+}
---
+++ Home_task_6/Exercise3/UniqueWordFinder.cs
@@ -0,0 +1,47 @@
+﻿namespace Exercise3;
+
+public static class UniqueWordFinder
+{
+    static readonly char[] splitChars = { ' ', ',', '.', '!', '?', '\n' };
+    public static IEnumerable<string> FindWords(string text)
+    {
+        string[] words = SplitText(text.ToLower());
+        List<string> uniqueWords = new List<string>();
+        uniqueWords.Add(words[0]);
+        yield return words[0];
+        bool isUnique;
+        for (int i = 1; i < words.Length; i++)
+        {
+            isUnique = true;
+            for (int j = 0; j < uniqueWords.Count; j++)
+            {
+                if (words[i].Equals(uniqueWords[j]))
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique)
+            {
+                uniqueWords.Add(words[i]);
+                yield return words[i];
+            }
+        }
+    }
+    public static IEnumerable<string> SetFindWordsMethod(string text)
+    {
+        string[] words = SplitText(text.ToLower());
+        HashSet<string> uniqueWords = new HashSet<string>();
+        foreach (string word in words)
+        {
+            if (uniqueWords.Add(word))
+            {
+                yield return word;
+            }
+        }
+    }
+    private static string[] SplitText(string text)
+    {
+        return text.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
