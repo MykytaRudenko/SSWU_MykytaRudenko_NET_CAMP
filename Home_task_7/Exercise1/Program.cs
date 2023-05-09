8514002e971cac2be206cbcf198569d0a9826560
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-09 13:43:09 +0300

    Update Program.cs

--- Home_task_7/Exercise1/Program.cs
+++ Home_task_7/Exercise1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Exercise1;
-
+// Було запропоновано реалізувати подійний механізм. Не побачила у вас.
+сумарний бал -90
 var context = new Context(new XStrategy(5000));
 context.StartTraffic();
 Console.WriteLine();
